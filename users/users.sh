31b4ec295a1cdc686616c0071fad41aa5d8dae81
Pedro Costa <pmdacosta@protonmail.com>
2025-08-24 14:32:06 +0100

    users.sh v3

--- users/users.sh
+++ users/users.sh
@@ -8,23 +8,35 @@
 #
 # v1: Show system username and full names separated by TAB
 # v2: Add support for -h option
+# v3: Add support for -V option and invalid option
 #
 # Pedro, August 2025
 #
 
 VERSION=2
-HELP_MSG="Usage: $0 [-h|-V]
+USAGE_MSG="Usage: $0 [-h | -V]"
+HELP_MSG="$USAGE_MSG
 
-    -h      Shows help message
-    -V      Show version
+  -h      Shows help message
+  -V      Show version
 "
 
 # Process command line options
-if test "$1" = "-h"
-then
-    echo "$HELP_MSG"
-    exit 0
-fi
+case "$1" in
+    -h)
+        echo "$HELP_MSG"
+        exit 0
+        ;;
+    -V)
+        echo "$0 version $VERSION"
+        exit 0
+        ;;
+    *)
+        echo "$0: Invalid option: $1
+Try '$0 -h' for more information."
+        exit 1
+        ;;
+esac
 
 # Process
 cut -d : -f1,5 /etc/passwd | tr : \\t
