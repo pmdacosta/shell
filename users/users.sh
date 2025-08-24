0542a5e1cfbbe72b27505607586f7778aebfe809
Pedro Costa <pmdacosta@protonmail.com>
2025-08-24 16:25:07 +0100

    users.sh v6

--- users/users.sh
+++ users/users.sh
@@ -13,57 +13,67 @@
 #     of $0 for the program name, -V extracts from header,
 #     add options for --help and --version.
 # v5: Add support for -s, --sort option.
+# v6: Add support for -r, --reverse, -u, --uppercase options,
+#     and support reading multiple options. Renamed sort to
+#     sorted to avoid conflict with the sort program.
 #
 # Pedro, August 2025
 #
 
 BASENAME="$(basename $0)"
-USAGE_MSG="Usage: $BASENAME [OPTION]"
+USAGE_MSG="Usage: $BASENAME [OPTIONS]"
 HELP_MSG="$USAGE_MSG
 
   -h, --help        Shows help message
+  -r, --reverse     Show in reverse order
   -s, --sort        Sort list
+  -u, --uppercase   Show list in uppercase
   -V, --version     Show version
 "
 INVALID_OPTION_MSG="$0: Invalid option: $1
 Try '$0 -h' for more information."
 
 # FLAGS
-sort=0      # sort output?
+sorted=0      # sort output?
+revert=0      # revert output order?
+uppercase=0   # print uppercase output?
 
 # Process command line options
-case "$1" in
-    -h | --help)
-        echo "$HELP_MSG"
-        exit 0
-        ;;
-    -V | --version)
-        # Grabs the latest version from the header. xargs will trim the whitespace
-        VERSION="$(grep '^# v' "$0" | tail -1 | cut -d : -f 1 | tr -d \# | xargs)"
-        echo "$BASENAME $VERSION"
-        exit 0
-        ;;
-    -s | --sort)
-        sort=1
-        ;;
-    *)
-        # -n option tests if the length of the string is non zero
-        if test -n "$1"
-        then
+while test -n "$1"
+do
+    case "$1" in
+        # check options
+        -r | --reverse) revert=1 ;;
+        -s | --sort) sorted=1 ;;
+        -u | --uppercase) uppercase=1 ;;
+        -h | --help)
+            echo "$HELP_MSG"
+            exit 0
+            ;;
+        -V | --version)
+            # Grabs the latest version from the header. xargs will trim the whitespace
+            VERSION=$(grep '^# v' "$0" | tail -1 | cut -d : -f 1 | tr -d \# | xargs)
+            echo "$BASENAME $VERSION"
+            exit 0
+            ;;
+        *)
+            # -n option tests if the length of the string is non zero
             echo "$INVALID_OPTION_MSG"
             exit 1
-        fi 
-        ;;
-esac
+            ;;
+    esac
+
+    # shift command line arguments $2 -> $1
+    shift
+done
 
 # Extract listing
 list=$(cut -d : -f1,5 /etc/passwd)
 
-# Order listing (if enabled)
-if test "$sort" = 1
-then
-    list=$(echo "$list" | sort)
-fi
+# Process enabled options
+test "$sorted" = 1 && list=$(echo "$list" | sort)           # sort
+test "$revert" = 1 && list=$(echo "$list" | tac)            # reverse order
+test "$uppercase" = 1 && list=$(echo "$list" | tr a-z A-Z)  # convert to uppercase
 
 # Print result
 echo "$list" | tr : \\t
