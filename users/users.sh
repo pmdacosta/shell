e1cb1ded961290a397fc147403729e8e79558689
Pedro Costa <pmdacosta@protonmail.com>
2025-08-24 14:11:10 +0100

    users.sh v2

--- users/users.sh
+++ users/users.sh
@@ -3,10 +3,29 @@
 # Based on Aurelio Vargas's usuarios.sh from the Shell Script Profissional Book
 # Adapted to english
 #
-# Shows the usernames on the system
+# Shows the usernames and their full name on the system
 # Obs.: Reads from /etc
+#
+# v1: Show system username and full names separated by TAB
+# v2: Add support for -h option
 #
 # Pedro, August 2025
 #
 
+VERSION=2
+HELP_MSG="Usage: $0 [-h|-V]
+
+    -h      Shows help message
+    -V      Show version
+"
+
+# Process command line options
+if test "$1" = "-h"
+then
+    echo "$HELP_MSG"
+    exit 0
+fi
+
+# Process
 cut -d : -f1,5 /etc/passwd | tr : \\t
+
